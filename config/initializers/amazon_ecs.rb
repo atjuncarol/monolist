f04da9cc29bb2961e8600a9cde2ab3ee8d580d3b
atjuncarol <atjuncarol@gmail.con>
2016-04-06 07:56:46 +0000

    first-commit

--- config/initializers/amazon_ecs.rb
+++ config/initializers/amazon_ecs.rb
@@ -1,5 +1,5 @@
 Amazon::Ecs.configure do |options|
-  options[:AKIAJHB3J77GYW3JADVQ] = Rails.application.secrets.aws_access_key_id
-  options[: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] = Rails.application.secrets.aws_secret_key
-  options[:athasjp-22] = Rails.application.secrets.associate_tag
+  options[:AWS_access_key_id] = Rails.application.secrets.aws_access_key_id
+  options[:AWS_secret_key]    = Rails.application.secrets.aws_secret_key
+  options[:associate_tag]     = Rails.application.secrets.associate_tag
 end
