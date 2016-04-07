6453e54868300e02a4e22b7316954c4cf5992eda
atjuncarol <atjuncarol@gmail.con>
2016-04-07 07:09:57 +0000

    07042016_2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,12 +8,12 @@ class ItemsController < ApplicationController
                                   :search_index => 'All' , 
                                   :response_group => 'Medium' , 
                                   :country => 'jp')
-      @amazon_items = response.items
+        @amazon_items = response.items
+      
     end
   end
 
   def show
-    
   end
   
   def item_params
